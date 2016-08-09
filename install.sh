d6f90ec1779f7a5832ca151ba2abc4cae2b55f28
Andrew Janke <andrew@apjanke.net>
2016-08-09 01:40:07 -0400

    convert indentation to spaces

--- install.sh
+++ install.sh
@@ -20,29 +20,29 @@ backdoor=$1
 
 pathdirs=`echo $PATH | tr : " "`
 for p in $pathdirs; do
-	set $p/perl $p/perl5*
-	while [ ! -z "$1" ]; do
-		if [ -x "$1" ]; then
-			echo Found perl in $1
-			perls="$perls $1"
-		fi
-		shift
-	done
+    set $p/perl $p/perl5*
+    while [ ! -z "$1" ]; do
+        if [ -x "$1" ]; then
+            echo Found perl in $1
+            perls="$perls $1"
+        fi
+        shift
+    done
 done
 for perl in $perls; do
-	if $perl -MText::Wrap -e0 >/dev/null 2>&1; then
-		echo Found a good perl in $perl
-		goodperls="$goodperls $perl"
-	fi
+    if $perl -MText::Wrap -e0 >/dev/null 2>&1; then
+        echo Found a good perl in $perl
+        goodperls="$goodperls $perl"
+    fi
 done
 echo The following perl executables will run cowsay:
 echo $goodperls
 echo I recommend the latest stable perl you can find.
 set $goodperls
 if [ -z "$1" ]; then
-	echo Ack!  You do not have Perl 5 installed correctly!
-	echo Get thee to CPAN!
-	exit 1
+    echo Ack!  You do not have Perl 5 installed correctly!
+    echo Get thee to CPAN!
+    exit 1
 fi
 usethisperl=$1
 echo I will be using $1 because I know it will work.
@@ -50,10 +50,10 @@ echo I will be using $1 because I know it will work.
 echo Now I need an installation prefix.  I will use /usr/local unless
 printf "you give me a better idea here: "
 if [ -n "$backdoor" ]; then
-	prefix=$backdoor
-	printf "%s (specified on command line)\n" $prefix
+    prefix=$backdoor
+    printf "%s (specified on command line)\n" $prefix
 else
-	read prefix
+    read prefix
 fi
 
 PREFIX=${prefix:-/usr/local}
